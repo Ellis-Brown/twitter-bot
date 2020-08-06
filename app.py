2bc715a712b9f8be235154a34e886157313e1d27
Ellis Brown <ellis.brown@tufts.edu>
2020-08-05 21:23:05 -0400

    Log into twitter with bot

---
+++ app.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import keys
+import time 
+
+class TwitterBot:
+    def __init__(self,username,password): #using __init__ is like a constructor
+        #You can get access to the datafields by calling self.(data)
+        self.username = username # Don't push these to git
+        self.password = password
+        self.bot = webdriver.Firefox() #Make sure you have firefox installed
+
+    def login(self):
+        bot = self.bot
+        bot.get('https://twitter.com/')
+        time.sleep(3) # lets the twitter page load up
+
+ellis = TwitterBot('enter@gmail.com', 'password')
+ellis.login()
